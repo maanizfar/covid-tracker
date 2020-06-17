2c7f4a32de3f8c68cae2523cf6a52ad7c90851a7
Salman Zafar <maanizfar@gmail.com>
2020-06-17 21:03:07 +0500

    create header

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { GoMarkGithub } from "react-icons/go";
+import { FaFacebook } from "react-icons/fa";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    ...theme.typography.h6,
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography className={classes.title}>Covid Tracker</Typography>
+        <IconButton>
+          <FaFacebook color="white" />
+        </IconButton>
+        <IconButton>
+          <GoMarkGithub color="white" />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
